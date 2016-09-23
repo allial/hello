3760ca6777d1bf4f5d3368f24f9ff3d98c0f6703
kostya <kostyatest@gmail.com>
2016-09-23 19:41:47 +0300

    second lesson done

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -33,26 +33,25 @@ AppAsset::register($this);
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post', ['class' => 'navbar-form'])
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
-    ]);
+	echo Nav::widget([
+	    'options' => ['class' => 'navbar-nav navbar-right'],
+	    'items' => [
+	        ['label' => 'Home', 'url' => ['/site/index']],
+	        [
+	           'label' => 'Status',
+	           'items' => [
+	                ['label' => 'Create', 'url' => ['/status/create']],
+	            ],
+	        ],                    
+	        ['label' => 'About', 'url' => ['/site/about']],
+	        ['label' => 'Contact', 'url' => ['/site/contact']],
+	        Yii::$app->user->isGuest ?
+	            ['label' => 'Login', 'url' => ['/site/login']] :
+	            ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+	                'url' => ['/site/logout'],
+	                'linkOptions' => ['data-method' => 'post']],
+	    ],
+	]);
     NavBar::end();
     ?>
 
